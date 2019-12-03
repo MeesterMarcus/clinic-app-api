94b50df03e50d8e12cfb1e0cc358b1aca2c872a7
MeesterMarcus <lorenzanamarcus@gmail.com>
2019-12-03 11:54:50 -0600

    moving package

--- src/main/java/com/mlorenzana/clinicappapi/services/PatientService.java
+++ src/main/java/com/mlorenzana/clinicappapi/services/PatientService.java
@@ -1,16 +1,18 @@
 package com.mlorenzana.clinicappapi.services;
 
-import com.mlorenzana.clinicappapi.models.PatientModel;
+
 import org.springframework.stereotype.Service;
+import com.mlorenzana.clinicappapi.models.PatientModel;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class PatientService {
 
     public List<PatientModel> getPatients() {
-        List <PatientModel> list = new ArrayList<>();
+        List<PatientModel> list = new ArrayList<>();
         list.add(new PatientModel(1L, "Jimmy", "Johnson", "1234 Addr", "San Antonio", "TX", "78232", "jj@gmail.com", "2102867939"));
         list.add(new PatientModel(1L, "Frank", "Johnson", "1234 Addr", "San Antonio", "TX", "78232", "jj@gmail.com", "2102867939"));
         list.add(new PatientModel(1L, "Tim", "Smith", "1234 Addr", "San Antonio", "TX", "78232", "jj@gmail.com", "2102867939"));
