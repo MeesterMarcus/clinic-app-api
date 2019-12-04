2050b9e11fa7c33c10776ba61bde3038b2b1d171
MeesterMarcus <lorenzanamarcus@gmail.com>
2019-12-03 21:20:32 -0600

    adding new entities for appointments and diagnosis

---
+++ src/main/java/com/mlorenzana/clinicappapi/entities/AppointmentEntity.java
@@ -0,0 +1,35 @@
+package com.mlorenzana.clinicappapi.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Data
+@Table(name = "appointments")
+public class AppointmentEntity {
+
+    @Id
+    @GeneratedValue(generator = "patient_generator")
+    @SequenceGenerator(
+            name = "patient_generator",
+            sequenceName = "patient_sequence",
+            initialValue = 1000
+    )
+    Long id;
+
+    @Column(name="appt_datetime")
+    ZonedDateTime dateTime;
+
+    @Column(name="notes")
+    String notes;
+
+    @Column(name="appt_type")
+    String apptType;
+
+    @ManyToOne
+    @JoinColumn(name="patientId", referencedColumnName = "id")
+    PatientEntity patientEntity;
+
+}
---
+++ src/main/java/com/mlorenzana/clinicappapi/entities/DiagnosisEntity.java
@@ -0,0 +1,37 @@
+package com.mlorenzana.clinicappapi.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Data
+@Table(name="diagnosis")
+public class DiagnosisEntity {
+
+    @Id
+    @GeneratedValue(generator = "patient_generator")
+    @SequenceGenerator(
+            name = "patient_generator",
+            sequenceName = "patient_sequence",
+            initialValue = 1000
+    )
+    Long id;
+
+    @Column(name="name")
+    String name;
+
+    @Column(name="description")
+    String description;
+
+    @Column(name="severity")
+    String severity;
+
+    @Column(name="date_diagnosed")
+    ZonedDateTime dateDiagnosed;
+
+    @ManyToOne
+    @JoinColumn(name="patientId", referencedColumnName = "id")
+    PatientEntity patientEntity;
+}
